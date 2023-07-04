b31b506a4467d6cbfc04011ff2405a28c91934f3
kanapiii <113953054+kanapiii@users.noreply.github.com>
2023-07-05 02:11:07 +0900

    svgでサイト完成

--- mypage/src/pages/home.js
+++ mypage/src/pages/home.js
@@ -1,19 +1,12 @@
 import style from '../style/home.module.css';
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
 
 const Home = () =>{
   return(
-    <>
     <div>
       <div>
+        <img className={style.img} src="/img/back.svg"></img>
       </div>
     </div>
-    </>
   );
 }
 export default Home;
